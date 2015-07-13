52eb4900491accb1a41f336b4331b47232d73cca
Anita Kuno <anteaya@anteaya.info>
2015-07-13 15:51:33 -0400

    New logrotate class in gerrit
    Add new class in gerrit to configure logrotation
    (initially for apache2 files).
    
    Change-Id: I2c456e7ef394417f52d5e75ad3fa04600bcaed34

---
+++ manifests/logrotate.pp
@@ -0,0 +1,27 @@
+# == Class: gerrit::logrotate
+#
+class gerrit::logrotate(
+  $options = [
+      'daily',
+      'missingok',
+      'rotate 30',
+      'compress',
+      'delaycompress',
+      'notifempty',
+      'create 640 root adm',
+      'sharedscripts',
+  ]
+) {
+  include ::logrotate
+
+  if $::osfamily  == 'RedHat' {
+    $apache_logdir = '/var/log/httpd'
+  }
+  else {
+    $apache_logdir = '/var/log/apache2'
+  }
+  ::logrotate::file { "gerrit_apache2":
+    log     => "${apache_logdir}/*.log",
+    options => $options,
+  }
+}

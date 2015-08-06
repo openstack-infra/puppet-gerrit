29e4422fd7f4ea5f263631770d4d084992f34faf
Yolanda Robla <yolanda.robla-mota@hp.com>
2015-08-06 15:38:22 +0200

    Allow to specify git directory to run the mirror repack
    This is a feature needed when local git directory
    is not on the expected gerrit directory.
    
    Change-Id: Ia3c424c236632ca983c8ab2fb85edfc4b60a84fe

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,13 +1,15 @@
 # == Class: gerrit::cron
 #
-class gerrit::cron {
+class gerrit::cron (
+    $local_git_dir = '/home/gerrit2/review_site/git/',
+) {
 
   cron { 'gerrit_repack':
     user        => 'gerrit2',
     weekday     => '0',
     hour        => '4',
     minute      => '7',
-    command     => 'find /home/gerrit2/review_site/git/ -type d -name "*.git" -print -exec git --git-dir="{}" repack -afd \;',
+    command     => "find ${local_git_dir} -type d -name \"*.git\" -print -exec git --git-dir=\"{}\" repack -afd \\;",
     environment => 'PATH=/usr/bin:/bin:/usr/sbin:/sbin',
   }
 

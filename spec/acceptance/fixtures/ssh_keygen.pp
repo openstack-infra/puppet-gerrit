8d8fddb7a3c511e6d0839e97b1d1ba94b870457d
Glauco Oliveira <gvinici@thoughtworks.com>
2015-08-19 14:36:16 -0300

    Add acceptance tests for puppet-gerrit
    Add acceptance tests for puppet-gerrit module so that once the module is
    applied we check if files were created, packages were installed and
    services are available to users.
    We left one pending test because this module is still not idempotent.
    
    Co-Authored-By: Caio Carrara <ccarrara@thoughtworks.com>
    Co-Authored-By: Dhiana Deva <ddeva@thoughtworks.com>
    Change-Id: I88b5730d369578ae6763226c0424467286e753a2

---
+++ spec/acceptance/fixtures/ssh_keygen.pp
@@ -0,0 +1,25 @@
+define create_ssh_key_directory() {
+  Exec { path => "/bin:/usr/bin" }
+  exec { "create temporary ${name} directory":
+    command => "mkdir -p ${name}",
+  }
+}
+
+define ssh_keygen (
+  $ssh_directory = undef
+) {
+  Exec { path => "/bin:/usr/bin" }
+
+  $ssh_key_file = "${ssh_directory}/${name}"
+
+  exec { "ssh-keygen for ${name}":
+    command => "ssh-keygen -t rsa -f $ssh_key_file -N ''",
+    creates => $ssh_key_file,
+  }
+}
+
+$ssh_key_directory = '/tmp/gerrit-ssh-keys'
+create_ssh_key_directory { "${ssh_key_directory}": }
+ssh_keygen {"ssh_rsa_key": ssh_directory => "${ssh_key_directory}" }
+ssh_keygen {"ssh_project_rsa_key": ssh_directory => "${ssh_key_directory}" }
+ssh_keygen {"ssh_replication_rsa_key": ssh_directory => "${ssh_key_directory}" }

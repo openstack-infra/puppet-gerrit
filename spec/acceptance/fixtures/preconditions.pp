6c399987049c4b2cc8deb01704a13b18881e7ae3
Glauco Oliveira <gvinici@thoughtworks.com>
2015-09-01 15:25:45 -0300

    Setup preconditions for acceptance tests.
    We split the definition of the module under test from its preconditions.
    
    As part of it, we have identified that we were installing the wrong pip
    (using python-pip package). The python-pip package has a version that is
    incompatible with puppet package provider. We changed it to install pip
    using get-pip utility, which is compatible with puppet.
    
    This behaviour was identified while testing puppet-zuul. To keep it
    consistent across projects, we decided to also apply the same logic on
    this module.
    
    Change-Id: I0b9014ce6470d5c142ab6a3b281ea7401f88e738
    Co-Authored-By: Bruno Tavares <btavare@thoughtworks.com>
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

---
+++ spec/acceptance/fixtures/preconditions.pp
@@ -0,0 +1,58 @@
+# Installing ssl-cert in order to get snakeoil certs
+package { 'ssl-cert':
+  ensure => present,
+}
+
+# workaround since pip is not being installed as part of this module
+exec { 'download get-pip.py':
+  command => 'wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py',
+  path    => '/bin:/usr/bin:/usr/local/bin',
+  creates => '/usr/local/bin/pip',
+}
+
+exec { 'install pip using get-pip':
+  command     => 'python /tmp/get-pip.py',
+  path        => '/bin:/usr/bin:/usr/local/bin',
+  refreshonly => true,
+  subscribe   => Exec['download get-pip.py'],
+}
+
+# method to create ssh directory
+define create_ssh_key_directory() {
+  Exec { path => '/bin:/usr/bin' }
+  exec { "create temporary ${name} directory":
+    command => "mkdir -p ${name}",
+  }
+}
+
+# method to generate key
+define ssh_keygen (
+  $ssh_directory = undef
+) {
+  Exec { path => '/bin:/usr/bin' }
+
+  $ssh_key_file = "${ssh_directory}/${name}"
+
+  exec { "ssh-keygen for ${name}":
+    command => "ssh-keygen -t rsa -f ${ssh_key_file} -N ''",
+    creates => $ssh_key_file,
+  }
+}
+
+$ssh_key_directory = '/tmp/gerrit-ssh-keys'
+file { $ssh_key_directory:
+  ensure => directory,
+}
+
+ssh_keygen {'ssh_rsa_key':
+  ssh_directory => $ssh_key_directory,
+  require       => File[$ssh_key_directory],
+}
+ssh_keygen {'ssh_project_rsa_key':
+  ssh_directory => $ssh_key_directory,
+  require       => File[$ssh_key_directory],
+}
+ssh_keygen {'ssh_replication_rsa_key':
+  ssh_directory => $ssh_key_directory,
+  require       => File[$ssh_key_directory],
+}
